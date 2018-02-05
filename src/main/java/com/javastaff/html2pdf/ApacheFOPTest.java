a9e704f4de82e4a4f749262915556d1a3cad4698
Federico <>
2018-02-05 21:11:58 +0100

    ApacheFOP

--- src/main/java/com/javastaff/html2pdf/ApacheFOPTest.java
+++ src/main/java/com/javastaff/html2pdf/ApacheFOPTest.java
@@ -1,5 +1,6 @@
 package com.javastaff.html2pdf;
 
+import java.io.ByteArrayInputStream;
 import javax.xml.transform.*;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
@@ -11,43 +12,48 @@ import org.apache.fop.apps.MimeConstants;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
-import java.net.URISyntaxException;
 import java.net.URL;
+import org.apache.fop.apps.FOPException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class ApacheFOPTest {
 
-    public static void main(String a[]) throws URISyntaxException, FileNotFoundException {
- 
+    public static void main(String a[]) throws Exception {
+        pdfTest("https://raw.githubusercontent.com/fpaparoni/html2pdf/master/html/simple.html","simple-fop.pdf");
+        pdfTest("https://raw.githubusercontent.com/fpaparoni/html2pdf/master/html/standard.html","standard-fop.pdf");
+        pdfTest("https://raw.githubusercontent.com/fpaparoni/html2pdf/master/html/css.html","css-fop.pdf");
+    }
+
+    private static void pdfTest(String url, String fileOut) 
+            throws FileNotFoundException, FOPException, 
+                   IOException, TransformerConfigurationException, 
+                   TransformerException {
+        String xsltfile = "https://raw.githubusercontent.com/fpaparoni/html2pdf/master/html/html2fo.xsl";
+        File pdffile = new File(fileOut);
+
+        FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+        FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+        OutputStream out = new java.io.FileOutputStream(pdffile);
+        out = new java.io.BufferedOutputStream(out);
+
         try {
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+            TransformerFactory factory = TransformerFactory.newInstance();
 
-            // Setup input and output files
-            String xmlfile = "https://raw.githubusercontent.com/fpaparoni/html2pdf/master/html/test.xml";
-            String xsltfile = "https://raw.githubusercontent.com/fpaparoni/html2pdf/master/html/test2fo.xsl";
-            File pdffile = new File("test-apachefop.pdf");
-
-            // configure fopFactory as desired
-            FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
-            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-            OutputStream out = new java.io.FileOutputStream(pdffile);
-            out = new java.io.BufferedOutputStream(out);
-
-            try {
-                Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
-                TransformerFactory factory = TransformerFactory.newInstance();
-                Transformer transformer = factory.newTransformer(new StreamSource(new URL(xsltfile).openStream()));
-                Source src = new StreamSource(new URL(xmlfile).openStream());
-                Result res = new SAXResult(fop.getDefaultHandler());
-                transformer.transform(src, res);
-            } finally {
-                out.close();
-            }
-
-            System.out.println("ApacheFOP test created");
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-            System.exit(-1);
+            Document d = Jsoup.parse(new URL(url),5000);
+            d.outputSettings().syntax(Document.OutputSettings.Syntax.xml);   
+            Transformer transformer = factory.newTransformer(new StreamSource(new URL(xsltfile).openStream()));
+            Source src = new StreamSource(new ByteArrayInputStream(d.outerHtml().getBytes()));
+            Result res = new SAXResult(fop.getDefaultHandler());
+            transformer.transform(src, res);
+        } finally {
+            out.close();
         }
+
+        System.out.println(fileOut+" created");
+        
     }
 }
